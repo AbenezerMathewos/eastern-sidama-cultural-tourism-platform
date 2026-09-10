f16dd596732c7cc4ec7f4e04a9bf28baf8afa5a0
Abenezer <mathewosa9@gmail.com>
2026-09-10 16:49:57 -0600

    feat: add date utility helpers for Ethiopian locale

---
+++ backend/utils/dateHelper.js
@@ -0,0 +1,4 @@
+﻿// Date utility helpers
+const formatDate = (date) => new Date(date).toLocaleDateString('en-ET');
+const isWeekend = (date) => [0, 6].includes(new Date(date).getDay());
+module.exports = { formatDate, isWeekend };
