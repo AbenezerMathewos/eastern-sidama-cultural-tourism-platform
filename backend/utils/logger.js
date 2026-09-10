9c7f75953af9b27a9e1054361ce9a73726f8947c
Abenezer <mathewosa9@gmail.com>
2026-09-10 16:50:00 -0600

    feat: add structured logger utility for backend

---
+++ backend/utils/logger.js
@@ -0,0 +1,11 @@
+﻿// Simple logger utility
+const log = (level, message) => {
+  const timestamp = new Date().toISOString();
+  console.log(\[\] [\] \\);
+};
+
+module.exports = {
+  info: (msg) => log('info', msg),
+  warn: (msg) => log('warn', msg),
+  error: (msg) => log('error', msg),
+};
