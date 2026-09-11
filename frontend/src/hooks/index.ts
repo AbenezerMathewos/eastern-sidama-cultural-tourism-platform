5c49731b8a8157e02a6c136e1a149408f9d9cdc7
Abenezer <mathewosa9@gmail.com>
2026-09-11 11:49:35 -0600

    feat: add barrel export index for custom hooks

---
+++ frontend/src/hooks/index.ts
@@ -0,0 +1,5 @@
+﻿// Barrel export for all custom hooks
+export { useDebounce } from './useDebounce';
+export { useLocalStorage } from './useLocalStorage';
+export { useWindowSize } from './useWindowSize';
+export { useScrollToTop } from './useScrollToTop';
