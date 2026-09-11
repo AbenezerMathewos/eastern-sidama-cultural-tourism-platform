fee80a9fd7bd0d99fb62569952ba22193aba168a
Abenezer <mathewosa9@gmail.com>
2026-09-11 11:43:33 -0600

    refactor: add JSDoc comments to catchAsync utility

--- backend/utils/catchAsync.js
+++ backend/utils/catchAsync.js
@@ -1,3 +1,10 @@
+﻿/**
+ * Wraps an async Express route handler and forwards any rejected promise
+ * to Express's next() error middleware, avoiding repetitive try/catch blocks.
+ *
+ * @param {Function} fn - Async function (req, res, next) => Promise.
+ * @returns {Function} Express middleware function.
+ */
 module.exports = fn => {
   return (req, res, next) => {
     fn(req, res, next).catch(next);
